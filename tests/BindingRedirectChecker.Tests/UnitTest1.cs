0d967df5a04f60a6f4862c6511da310e61878bf2
Doug Murphy <doug.murphy82@gmail.com>
2021-06-08 19:52:12 -0400

    Create stubbed tests project.

---
+++ tests/BindingRedirectChecker.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace BindingRedirectChecker.Tests {
+    public class Tests {
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
